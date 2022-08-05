efabbfa707671a311ad4f9fad1904850b7bc6acb
wilson148 <soetomowilson@gmail.com>
2022-08-05 12:06:48 -0700

    adding new routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,26 +1,121 @@
-const router = require('express').Router();
-const { User, Thought } = require('../../models');
-const filter = {}
+const router = require("express").Router();
+const { User, Thought } = require("../../models");
+const filter = {};
 
-router.get('/', (req, res) => {
-    Thought.find().then(thoughts => {
-        res.json(thoughts);
-    }).catch((err)=>{
+router.get("/", (req, res) => {
+  Thought.find()
+    .then((thoughts) => {
+      res.json(thoughts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+router.post("/", (req, res) => {
+  Thought.create(req.body)
+    .then((thought) => {
+      User.findOneAndUpdate({});
+      res.json(thought);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//`GET` to get a single thought by its `_id`
+router.get("/:id",(req,res) =>{
+    Thought.findById(req.params.id)
+    .then((thought) => {
+        res.json(thought);
+    }).catch((err) => {
         console.log(err);
         res.status(500).json(err);
-    })
+    });
 });
-router.post('/', (req, res) => {
-    Thought.create(req.body).then(thought => {
-        User.findOneAndUpdate({})
-        res.json(thought);
-    }).catch((err)=>{
+
+//POST` to create a new thought (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
+router.post("/:id", (req, res) => {
+    Thought.create(req.body)
+    .then((thought) => {
+        User.findByIdAndUpdate(req.params.id, {$push: {thoughts: thought._id}})
+        .then(() => {
+            res.json(thought);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        }
+        );
+    }).catch((err) => {
         console.log(err);
         res.status(500).json(err);
     }
-    )
+    );
+}
+);
+
+//`PUT` to update a thought by its `_id`
+
+router.put("/:id", (req,res) =>{
+    Thought.findByIdAndUpdate(req.params.id,req.body)
+    .then((thought) => {
+        res.json(thought);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    );
+})
+
+//`DELETE` to remove a thought by its `_id`
+
+router.delete("/:id", (req,res) =>{
+    Thought.findByIdAndRemove(req.params.id)
+    .then((thought) => {
+        res.json(thought);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    );
 });
 
+//`/api/thoughts/:thoughtId/reactions`
 
+router.post("/:thoughtId/reactions", (req,res) =>{
+    Thought.findByIdAndUpdate(req.params.thoughtId, {$push: {reactions: req.body}})
+    .then((thought) => {
+        res.json(thought);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    );
+});
+
+//`POST` to create a reaction stored in a single thought's `reactions` array field
+router.post("/:thoughtId/reactions", (req,res) =>{
+    Thought.create(req.params.thoughtId, {$push: {reactions: req.body}})
+    .then((thought) => {
+        res.json(thought);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    );
+});
+
+//DELETE` to pull and remove a reaction by the reaction's `reactionId` value
+router.delete("/:thoughtId/reactions/:reactionId", (req,res) =>{
+    Thought.findByIdAndUpdate(req.params.thoughtId, {$pull: {reactions: {_id: req.params.reactionId}}}).then((thought) => {
+        res.json(thought);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    );
+});
 
 module.exports = router;
+
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -18,6 +18,42 @@ router.post('/', (req, res) => {
     }
     )
 });
+//`GET` a single user by its `_id` and populated thought and friend data
+router.get("/:id", (req, res) => {
+    User.findById(req.params.id)
+      .populate("thought")
+      .then((thought) => {
+        res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  //`PUT` to update a user by its `_id`
+  router.put("/:id", (req, res) => {
+      User.findByIdAndUpdate(req.params.id, req.body)
+      .then((user) => {
+          res.json(user);
+      }).catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      }
+      );
+  }
+  );
+  //`DELETE` to remove user by its `_id`
+    router.delete("/:id", (req, res) => {
+        User.findByIdAndRemove(req.params.id)
+        .then((user) => {
+            res.json(user);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        }
+        );
+    }
+    );
 
 
 
